382b58e13af021f62473372577aabe12bd107e32
zaheer <zaheermalik284@gmail.com>
2019-06-13 17:58:32 +0500

    form validations in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  appointmentForm = new FormGroup({
+    name: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required]),
+    phone: new FormControl('',[Validators.required]),
+    city: new FormControl('',[Validators.required]),
+    date: new FormControl('',[Validators.required]),
+    age: new FormControl('',[Validators.required,Validators.min(18)]),
+    message: new FormControl('',[Validators.required])
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.appointmentForm.value);
   }
 
 }
